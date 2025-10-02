707c2786bab8fc5f6302d77a9b6438783b293b37
Oziel <ozielll.2000@gmail.com>
2025-10-02 15:19:51 -0400

    Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,10 +8,17 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
+function images() {
+    return gulp.src('./images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
 // função que observa mudanças nos arquivos
 function watchFiles() {
     gulp.watch('./src/styles/*.scss', styles);
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = gulp.series(styles, watchFiles);
